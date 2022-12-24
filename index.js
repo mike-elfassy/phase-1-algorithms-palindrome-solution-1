0a2b95f3a1a7294be35f0df01be52e4d125f3180
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-24 10:35:14 -0700

    commit tree d876145a91e042b64fb2257214276f76e40eaad4

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  
+  reverse the input string
+  if the reversed string is the same as the input
+    return true
+  else
+    return false 
 */
 
 /*
@@ -15,6 +30,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
